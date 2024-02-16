a957c22c057bec8f2e419a36355188e1e667f282
Enrico Reimer <reimer@cbs.mpg.de>
2024-02-16 18:11:26 +0100

    add parameter for release command

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use md5::Context;
 use std::path::{Path, PathBuf};
 use tokio::{fs::File, io::AsyncReadExt, task::JoinSet};
 use std::error::Error;
-use clap::{Parser,ValueHint::FilePath};
+use std::process::Command;
+use clap::{Parser, ValueHint::FilePath};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -13,7 +14,10 @@ struct Cli {
     file:Vec<PathBuf>,
     /// maximum size of files active at the same time (in GBytes)
     #[arg(short,long,default_value_t=1024)]
-    max_size:u64
+    max_size:u64,
+    ///release command
+    #[arg(long)]
+    release:Option<String>
 }
 
 async fn check_file(path:PathBuf, reference:String) -> std::io::Result<bool>
@@ -43,14 +47,21 @@ async fn check_file(path:PathBuf, reference:String) -> std::io::Result<bool>
 struct Reader
 {
     readers:JoinSet<(PathBuf,std::io::Result<bool>)>,
+    release:Vec<String>,
     cur_size:u64,max_size:u64
 }
 
 impl Reader
 {
-    fn new(max_size:u64)->Reader
+    fn new(max_size:u64, release:Option<String>)->Reader
     {
-        Reader{max_size,..Default::default()}
+        let release= match release {
+            None => vec![],
+            Some(r) => {
+                r.split_whitespace().map(String::from).collect()
+            }
+        };
+        Reader{max_size,release,..Default::default()}
     }
     async fn add<T>(&mut self,path:T, reference:String) -> Result<(),Box<dyn Error>> where T:AsRef<Path>
     {
@@ -78,15 +89,24 @@ impl Reader
         {
             None => Ok(None),
             Some((path,Ok(ok))) =>
-                {
-                    self.cur_size -= path.metadata()?.len();
-                    println!("{} {}",path.to_string_lossy(),if ok {"OK"} else {"FAIL"});
-                    Ok(Some((path,ok)))
-                }
+            {
+                self.cur_size -= path.metadata()?.len();
+                println!("{} {}",path.to_string_lossy(),if ok {"OK"} else {"FAIL"});
+                self.release(&path);
+                Ok(Some((path,ok)))
+            }
             Some((path,Err(e))) => {
+                self.release(&path);
                 Err(format!(r#"failed reading {}: {e}"#,path.to_string_lossy()).into())
             }
         }
+    }
+    fn release<T>(&self,path:T) where T:AsRef<Path>
+    {
+        let (program,params)=self.release.split_first().unwrap();
+        Command::new(program).args(params)
+            .arg(path.as_ref().as_os_str())
+            .status().ok();
     }
     async fn join(&mut self) -> Result<(),Box<dyn Error>>
     {
@@ -99,7 +119,7 @@ impl Reader
 async fn main() -> Result<(),Box<dyn Error>>
 {
     let args = Cli::parse();
-    let mut reader = Reader::new(args.max_size^30);
+    let mut reader = Reader::new(args.max_size^30,args.release);
 
     for md5filepath in args.file
     {
